bdf34c3784b423ee1572e7fdb712dcb7f478a853
Nikita Rozanov <nikitaroz@gmail.com>
2020-11-23 16:23:35 -0800

    added additional ui elements

--- app/app.py
+++ app/app.py
@@ -7,7 +7,6 @@ if module_path not in sys.path:
 
 from common.database_connector import DatabaseConnector
 from flask import Flask, jsonify, render_template, request
-from numpy.random import randint
 
 db = DatabaseConnector("../db/spam.db", None, None)
 app = Flask(__name__)
@@ -19,19 +18,33 @@ def main():
     return render_template("main.html")
 
 
-@app.route("/get_email")
+@app.route("/get_email", methods=["POST"])
 def get_email():
-
-    value = randint(1, 100)
-    subject, body = db.cursor.execute(
-        "SELECT subject, body FROM messages where message_id=?", (value,)
-    ).fetchone()
-
-    return jsonify(subject=subject, body=body)
+    if request.method == "POST":
+        labels = []
+        if request.args.get("hasNormal", True): labels.append(0)
+        if request.args.get("hasSpam", True): labels.append(1)
+        prob_min = request.args.get("sliderMin", 0) / 100
+        prob_max = request.args.get("sliderMax", 1) / 100
+    
+        if len(labels) == 0:
+            return ("", 204)
+        query = f"SELECT subject, body FROM messages WHERE  " + \
+            f"(prob_spam BETWEEN ? AND ?) AND " + \
+            f"(label IN ({', '.join(len(labels)*['?'])})) " + \
+            f"ORDER BY RANDOM()"
+        result = db.cursor.execute(
+            query,
+            (prob_min, prob_max, *labels)
+        ).fetchone()
+        if result is None:
+            return ("", 204)
+        subject, body = result
+        return jsonify(subject=subject, body=body)
 
 
 # https://stackoverflow.com/questions/52904116/flask-use-the-same-view-to-render-a-search-form-and-then-search-results/52905054
-@app.route("/search", methods=["GET", "POST"])
+@app.route("/search", methods=["GET"])
 def search():
     # TODO: validate input
     if request.method == "GET":
@@ -48,6 +61,10 @@ def search():
 @app.route("/about")
 def about_page():
     return render_template("about.html")
+
+@app.route("/classifier")
+def classifier_page():
+    return render_template("classifier.html")
 
 
 if __name__ == "__main__":
